23ba515a22eb5b4129114537dfdec45c16244612
Benjamin-Riordan <>
2023-05-20 18:20:10 +0000

    imported questions arrrys from 3rd party source

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,157 @@
+const levels = [
+    {
+      difficulty: "level-1",
+      questions: [
+        {
+          question: "Who is Luke Skywalker's father?",
+          options: ["Darth Vader", "Obi-Wan Kenobi", "Yoda", "Han Solo"],
+          answer: "Darth Vader"
+        },
+        {
+          question: "What is the name of Han Solo's ship?",
+          options: ["Millennium Falcon", "Star Destroyer", "X-wing", "TIE Fighter"],
+          answer: "Millennium Falcon"
+        },
+        {
+          question: "Which planet is home to Chewbacca and the Wookiees?",
+          options: ["Kashyyyk", "Tatooine", "Hoth", "Endor"],
+          answer: "Kashyyyk"
+        },
+        {
+          question: "Who is the famous Jedi Master that trained Obi-Wan Kenobi?",
+          options: ["Qui-Gon Jinn", "Mace Windu", "Yoda", "Anakin Skywalker"],
+          answer: "Yoda"
+        },
+        {
+          question: "What is the name of the bounty hunter who captures Han Solo?",
+          options: ["Boba Fett", "Jango Fett", "Cad Bane", "Dengar"],
+          answer: "Boba Fett"
+        }
+      ]
+    },
+    {
+      difficulty: "level-2",
+      questions: [
+        {
+          question: "Who is the supreme leader of the First Order?",
+          options: ["Snoke", "Kylo Ren", "General Hux", "Captain Phasma"],
+          answer: "Snoke"
+        },
+        {
+          question: "What is the weapon used by Jedi and Sith called?",
+          options: ["Lightsaber", "Blaster", "Ion Cannon", "Thermal Detonator"],
+          answer: "Lightsaber"
+        },
+        {
+          question: "Who is the smuggler turned Resistance general in the sequel trilogy?",
+          options: ["Poe Dameron", "Lando Calrissian", "Finn", "Leia Organa"],
+          answer: "Poe Dameron"
+        },
+        {
+          question: "Which famous ship made the Kessel Run in less than 12 parsecs?",
+          options: ["Millennium Falcon", "Death Star", "Star Destroyer", "X-wing"],
+          answer: "Millennium Falcon"
+        },
+        {
+          question: "What is the name of the desert planet where Rey is first discovered?",
+          options: ["Jakku", "Tatooine", "Naboo", "Geonosis"],
+          answer: "Jakku"
+        }
+      ]
+    },
+    {
+      difficulty: "level-3",
+      questions: [
+        {
+          question: "Which Sith Lord is the apprentice of Darth Sidious?",
+          options: ["Darth Maul", "Darth Tyranus", "Darth Vader", "Darth Plagueis"],
+          answer: "Darth Maul"
+        },
+        {
+          question: "What is the capital planet of the Galactic Republic?",
+          options: ["Coruscant", "Naboo", "Alderaan", "Kamino"],
+          answer: "Coruscant"
+        },
+        {
+          question: "Who is the leader of the Separatist Alliance during the Clone Wars?",
+          options: ["Count Dooku", "General Grievous", "Nute Gunray", "Asajj Ventress"],
+          answer: "Count Dooku"
+        },
+        {
+          question: "What is the name of Anakin Skywalker's Padawan in the Clone Wars?",
+          options: ["Ahsoka Tano", "Shaak Ti", "Barriss Offee", "Luminara Unduli"],
+          answer: "Ahsoka Tano"
+        },
+        {
+          question: "Which bounty hunter did Jango Fett request to be the template for the clone army?",
+          options: ["Boba Fett", "Dengar", "Zam Wesell", "Cad Bane"],
+          answer: "Boba Fett"
+        }
+      ]
+    },
+    {
+      difficulty: "level-4",
+      questions: [
+        {
+          question: "Who killed Jabba the Hutt?",
+          options: ["Princess Leia", "Luke Skywalker", "Han Solo", "Lando Calrissian"],
+          answer: "Princess Leia"
+        },
+        {
+          question: "What is the name of the ancient Sith homeworld?",
+          options: ["Korriban", "Moraband", "Exegol", "Malachor"],
+          answer: "Korriban"
+        },
+        {
+          question: "Who is the mastermind behind the creation of the Death Star?",
+          options: ["Galen Erso", "Wilhuff Tarkin", "Orson Krennic", "Darth Sidious"],
+          answer: "Galen Erso"
+        },
+        {
+          question: "Which Jedi Master ignited their lightsaber in Attack of the Clones?",
+          options: ["Mace Windu", "Yoda", "Obi-Wan Kenobi", "Kit Fisto"],
+          answer: "Obi-Wan Kenobi"
+        },
+        {
+          question: "What is the name of the species that Yoda belongs to?",
+          options: ["Yoda's species", "Dagobah", "Kashyyyk", "Kamino"],
+          answer: "Yoda's species"
+        }
+      ]
+    },
+    {
+      difficulty: "level-5",
+      questions: [
+        {
+          question: "Who killed Supreme Leader Snoke?",
+          options: ["Kylo Ren", "Rey", "Luke Skywalker", "Darth Vader"],
+          answer: "Kylo Ren"
+        },
+        {
+          question: "What is the name of Darth Vader's fortress on Mustafar?",
+          options: ["Fortress Vader", "Castle Ren", "Sith Citadel", "Mustafar Stronghold"],
+          answer: "Mustafar Stronghold"
+        },
+        {
+          question: "Who is the creator of the Mandalorian series?",
+          options: ["Jon Favreau", "George Lucas", "Dave Filoni", "Rian Johnson"],
+          answer: "Jon Favreau"
+        },
+        {
+          question: "What is the name of the planet where the final battle in The Rise of Skywalker takes place?",
+          options: ["Exegol", "Ahch-To", "Crait", "Endor"],
+          answer: "Exegol"
+        },
+        {
+          question: "Which Jedi Master survived Order 66?",
+          options: ["Yoda", "Obi-Wan Kenobi", "Mace Windu", "Qui-Gon Jinn"],
+          answer: "Obi-Wan Kenobi"
+        }
+      ]
+    }
+  ];
+
+
 document.addEventListener("DOMContentLoaded",function(){
     lauchQuiz()
 })
@@ -59,9 +213,4 @@ function updateScore(){
 }
 
 function displayQuestion(level){
-
-
-}
-
-function levelSelect(){
 }
