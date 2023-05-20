39367f2efbc5842ffd8bc61deb8ba80a29972a0d
Benjamin-Riordan <>
2023-05-20 14:56:42 +0000

    updating welcome user fucntion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,25 @@
+document.addEventListener("DOMContentLoaded",function(){
+    lauchQuiz()
+})
+
+function consoleTester(){
+    console.log("this logic works")
+}
+
 function lauchQuiz(){
-    
+    let quizLaunched = document.getElementById("launch-Btn")
+    quizLaunched.addEventListener("click",function(){
+        welcomeUser()
+    });
 }
 
 function welcomeUser(){
-    
+    let gameArea = document.getElementById("game-area")
+    gameArea.innerHTML = `
+    <header id="new-header"> Hello and wellcome to the Star Wars quiz!</header>
+    <p id ="game-info">This quiz will test you knowlage, it has 5 diffculty levels and there are some very hard questions "<p>
+    <p id ="game-info> Are toy ready to take the quiz<p>
+    `
 }
 
 function updateScore(){
