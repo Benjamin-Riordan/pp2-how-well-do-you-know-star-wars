372b8719b3cd38205b320b4542e44eb409a122eb
Benjamin-Riordan <>
2023-05-20 17:36:21 +0000

    no chnages

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,8 @@ function lauchQuiz(){
     let quizLaunched = document.getElementById("launch-Btn")
     quizLaunched.addEventListener("click",function(){
         welcomeUser();
-        getUsername();
+        getUsername()
+        levelSelect();
     });
 }
 
@@ -23,18 +24,26 @@ function welcomeUser(){
     <p class ="game-info"> Are you ready to take the quiz? </p>
     <p class ="game-info"> please pick your level:</p>
         <ul id =level-selction">
-            <li><button class ="Btn-style" id ="level-1" >Level 1(You cant get these wrong)</button>
+            <li><button class ="Btn-style" id ="1" >Level 1(You cant get these wrong)</button>
             </li>
-            <li><button class ="Btn-style" id ="level-2" >Level 2(You might get one wrong)</button>
+            <li><button class ="Btn-style" id ="2" >Level 2(You might get one wrong)</button>
             </li>
-            <li><button class ="Btn-style" id ="level-3" >Level 3(ok you will get one wrong)</button>
+            <li><button class ="Btn-style" id ="3" >Level 3(ok you will get one wrong)</button>
             </li>
-            <li><button class ="Btn-style" id ="level-4" >Level 4(ill be fair this is very diffcult)</button>
+            <li><button class ="Btn-style" id ="4" >Level 4(ill be fair this is very diffcult)</button>
             </li>
-            <li><button class ="Btn-style" id ="level-5" >Level 5(you need to be a jedi master to get these right)</button>
+            <li><button class ="Btn-style" id ="5" >Level 5(you need to be a jedi master to get these right)</button>
             </li>
         </ul>
     `
+}
+
+function levelSelect(){
+    let levelBtn = document.getElementsByClassName("Btn-style")
+    for (button of levelBtn){
+
+    }
+
 }
 
 function updateScore(){
