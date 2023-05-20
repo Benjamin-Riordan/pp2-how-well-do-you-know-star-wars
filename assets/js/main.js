c824184d5a54192e51b31ddbe0a3e3b073d6aea8
Benjamin-Riordan <>
2023-05-20 15:17:33 +0000

    added in level selction and a bit of sytling

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,8 +17,21 @@ function welcomeUser(){
     let gameArea = document.getElementById("game-area")
     gameArea.innerHTML = `
     <header id="new-header"> Hello and wellcome to the Star Wars quiz!</header>
-    <p id ="game-info">This quiz will test you knowlage, it has 5 diffculty levels and there are some very hard questions "<p>
-    <p id ="game-info> Are toy ready to take the quiz<p>
+    <p class ="game-info">This quiz will test you knowlage, it has 5 diffculty levels and there are some very hard questions!<p>
+    <p class ="game-info"> Are you ready to take the quiz? </p>
+    <p class ="game-info"> please pick your level:</p>
+        <ul id ="level-selction">
+            <li><button class ="Btn-style" id ="level-1" >Level 1(You cant get these wrong)</button>
+            </li>
+            <li><button class ="Btn-style" id ="level-2" >Level 2(You might get one wrong)</button>
+            </li>
+            <li><button class ="Btn-style" id ="level-3" >Level 3(ok you will get one wrong)</button>
+            </li>
+            <li><button class ="Btn-style" id ="level-4" >Level 4(ill be fair this is very diffcult)</button>
+            </li>
+            <li><button class ="Btn-style" id ="level-5" >Level 5(you need to be a jedi master to get these right)</button>
+            </li>
+        </ul>
     `
 }
 
